7b2858424c692484952bb73bba6f975dffb489e3
Alex Story <astory11@yahoo.com>
2024-04-29 12:48:52 -0500

    Adds better config setup, logging support

--- src/Config.fs
+++ src/Config.fs
@@ -3,6 +3,7 @@ module FSRun.Config
 open System.Collections.Generic
 open System.IO
 open Tomlyn
+open System.Text.RegularExpressions
 
 type CommandAction =
     | Steps of string list
@@ -29,24 +30,51 @@ type FSCommandInterop = {
 }
 
 
+type CommandValue =
+    | SimpleCommand of string
+    | StepsCommand of string list
+    | DetailedCommand of FSCommandInterop
+
+
 [<CLIMutable>]
 type FSConfigInterop = {
     Environment: Dictionary<string, string>
-    Commands: ResizeArray<FSCommandInterop>
+    Commands: Dictionary<string, CommandValue>
 }
+
+
+let parseSimpleCommand (cmdLine: string) name =
+    let regex = new Regex("""[ ](?=(?:[^""]*""[^""]*"")*[^""]*$)""")
+    let parts = regex.Split(cmdLine)
+    { Name = name; Action = Command (parts.[0], (parts.[1..] |> Array.toList)) }
+
 
 let fromFile filePath : FSConfig =
     let file = File.ReadAllText(filePath)
-    let configInterop = Toml.ToModel<FSConfigInterop>(file)
-    let environment = configInterop.Environment |> Seq.map (fun kvp -> kvp.Key, kvp.Value) |> Map.ofSeq
-    let commands = 
-        configInterop.Commands |> Seq.map (fun cmd -> 
-            let action = 
-                if not (isNull cmd.Command) then
-                    Command (cmd.Command, cmd.Args |> List.ofSeq)
-                else
-                    Steps (cmd.Steps |> List.ofSeq)
-            { Name = cmd.Name; Action = action }
-        )
-        |> List.ofSeq
-    { Environment = environment; Commands = commands }
+    let configInterop = Toml.ToModel(file)
+    let tomlEnv: IDictionary<string, obj> = configInterop["environment"] :?> IDictionary<string, obj>
+    let tomlCommands = configInterop["commands"] :?> Model.TomlTable
+    let env = tomlEnv |> Seq.map (fun kvp -> kvp.Key, kvp.Value |> string) |> Map.ofSeq
+    let commands =
+        tomlCommands |> Seq.map (fun kvp -> 
+            let name = kvp.Key
+            let value = kvp.Value
+            let commandValue = 
+                match value with
+                | :? string -> SimpleCommand ((string) value)
+                | :? Model.TomlArray as arr -> StepsCommand (arr |> Seq.cast<string> |> List.ofSeq)
+                | :? Model.TomlTable as tbl -> 
+                    let cmd = if tbl.ContainsKey("command") then tbl["command"] :?> string else ""
+                    let args = if tbl.ContainsKey("args") then tbl["args"] :?> IList<obj> |> Seq.cast<string> |> List.ofSeq else []
+                    let steps = if tbl.ContainsKey("steps") then tbl["steps"] :?> IList<obj> |> Seq.cast<string> |> List.ofSeq else []
+                    DetailedCommand { Name = name; Command = cmd; Args = args |> ResizeArray; Steps = steps |> ResizeArray }
+                | _ -> failwith "Invalid command value"
+            
+            match commandValue with
+            | SimpleCommand cmd -> parseSimpleCommand cmd name
+            | StepsCommand steps -> { Name = name; Action = Steps steps }
+            | DetailedCommand cmd -> { Name = name; Action = Command (cmd.Command, cmd.Args |> List.ofSeq) }
+            
+        ) |> List.ofSeq
+
+    { Environment = env; Commands = commands }
--- src/Program.fs
+++ src/Program.fs
@@ -1,6 +1,7 @@
 ﻿open Argu
 open Fli
 open FSRun
+open System.IO
 
 
 
@@ -10,6 +11,7 @@ type CliCommand =
     | [<MainCommand; Unique>] Name of string
     | [<AltCommandLine("-q")>] Quiet
     | List
+    | Logs of path: string
 
     interface IArgParserTemplate with
         member s.Usage =
@@ -17,12 +19,26 @@ type CliCommand =
             | Name name -> $"Name of the command to run"
             | Quiet -> $"Don't print command's output"
             | List -> $"List all commands"
+            | Logs path -> $"Log output to a file at the given path"
 
 
 type Action =
     | PrintUsage
     | ListCommands
     | RunCommand
+
+
+type Settings = {
+    Quiet: bool
+    LogPath: string option
+    Command: string option
+}
+
+let parseSettings (results: ParseResults<CliCommand>) =
+    let quiet = results.Contains Quiet
+    let logPath = results.TryGetResult(Logs)
+    let command = results.TryGetResult(Name)
+    { Quiet = quiet; LogPath = logPath; Command = command}
 
 
 let getAction (results: ParseResults<CliCommand>)  =
@@ -36,13 +52,13 @@ let getAction (results: ParseResults<CliCommand>)  =
         PrintUsage
 
 
-let rec runCommand (name: string) (config: Config.FSConfig) (results: ParseResults<CliCommand>) =
+let rec runCommand (name: string) (config: Config.FSConfig) settings =
     match List.tryFind (fun (cmd: Config.FSCommand) -> cmd.Name = name) config.Commands with
     | Some cmd ->
         match cmd.Action with
         | Config.CommandAction.Steps steps ->
             steps
-            |> List.map (fun step -> runCommand step config results)
+            |> List.map (fun step -> runCommand step config settings)
             |> List.fold (fun acc code -> if acc = 0 then code else acc) 0
         | Config.CommandAction.Command (cmd, args) ->
             let result =
@@ -52,15 +68,17 @@ let rec runCommand (name: string) (config: Config.FSConfig) (results: ParseResul
                     EnvironmentVariables (config.Environment |> Map.toList)
                 }
                 |> Command.execute
-            if not (results.Contains Quiet) then
+            if not (settings.Quiet) then
                 result |> Output.printText
+            if settings.LogPath.IsSome then
+                File.WriteAllText(settings.LogPath.Value, result.Text.Value)
             result |> Output.toExitCode
     | None ->
         printf $"Command '{name}' not found\n"
         1
 
 
-let performAction action (config: Config.FSConfig) (parser: ArgumentParser<CliCommand>) (results: ParseResults<CliCommand>) =
+let performAction action (config: Config.FSConfig) (parser: ArgumentParser<CliCommand>) settings =
     match action with
     | PrintUsage -> 
         printf $"{parser.PrintUsage()}\n"
@@ -70,8 +88,7 @@ let performAction action (config: Config.FSConfig) (parser: ArgumentParser<CliCo
             printf $"- {cmd.Name}\n"
         0
     | RunCommand ->
-        let name = results.GetResult(Name, "default")
-        runCommand name config results
+        runCommand settings.Command.Value config settings
 
 
 let parser = ArgumentParser.Create<CliCommand>(programName="fsrun")
@@ -82,4 +99,5 @@ let main argv =
     let results = parser.ParseCommandLine(argv, raiseOnUsage=false)
     let config = Config.fromFile "fsrun.toml"
     let action = getAction results
-    performAction action config parser results
+    let settings = parseSettings results
+    performAction action config parser settings
